4c5f27be40e5f4fa24d1f2308c68485abd399d71
Dylan Callahan <45744428+sixbrigands@users.noreply.github.com>
2019-11-09 07:19:24 -0500

    added texting function with twilio
    must replace sid and auth with actual numbers to use

--- mongoparser.py
+++ mongoparser.py
@@ -1,31 +1,48 @@
 
 from pymongo import MongoClient
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "***************************"
+# Your Auth Token from twilio.com/console
+auth_token  = "**************************"
+client = Client(account_sid, auth_token)
 
 #this just connects to local host (27017) by default.
-client = MongoClient()
+dbclient = MongoClient()
 
-#get the database from the client, now it's "testdb"
-db = client.db
+#get the database from the client, now it's "db"
+db = dbclient.db
 
-#get the collection from the db, it's the same name
+#get the collection from the db (users)
 collection = db.users
 
-bigDict = {}
-
-def buildDict():
+def dictBuidler():
+	bigDict = {}
 	for json in collection.find():  #go through all jsons in collection
-		print(json)
 		for key in json["keywords"]: #iterate through array of keywords (key is a keyword)
 			if key in bigDict: #check if key is already in dict 
 				bigDict[key].append(json["phone"]) #access the phone number array and append a new one
 			else:
 				phoneList = [json["phone"]]
 				bigDict.update({key: phoneList}) #add a new keyword:phone number array pair
-	print(bigDict)
+	return bigDict
 
-buildDict()
+def main():
+	bigDict = dictBuidler()
+	transcript = ["Dylan"] #example transcript for testing
+
+	for v in transcript: #iterate through strings in transcript
+		if v in bigDict: #if a string is one of the keywords
+  			for pn in bigDict[v]: #interate through list of phone numbers
+  				message = client.messages.create( # text all phone numbers about their keyword being mentioned.
+    				to="+" + pn, 
+    				from_="+12562697171",
+   					body="Your keyword " + v + " was mentioned")
 
 
 
 
 
+if __name__ == '__main__':
+	main()
